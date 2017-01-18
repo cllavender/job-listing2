79d73217d8d2235db1bc24efc84460639cff1ba4
elizachen <elizachen34@gmail.com>
2017-01-18 23:19:01 +0800

    Gemfile for Heroku & seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,15 +7,25 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 puts "這個種子檔會自動建立一個admin帳號, 並且創建 10 個 public jobs, 以及10個hidden jobs"
 
-create_account = User.create([email: 'example@gmail.com', password: '12345678', password_confirmation: '12345678', is_admin: 'true'])
+create_account = User.create([email: 'elizachen34@gmail.com', password: '888888', password_confirmation: '888888', is_admin: 'true'])
 puts "Admin account created."
 
-create_jos = for i in 1..10 do
-  Job.create!([title: "Job no.#{i}", description: "這是用種子建立的第 #{i} 個Public工作", wage_upper_bound: rand(50..99)*100, wage_lower_bound: rand(10..49)*100, is_hidden: "false"])
+create_jos = for i in 1..3 do
+  Job.create!([title: "Job no.#{i} 帮买星巴克摩卡", description: "這是用種子建立的第 #{i} 個Public工作", wage_upper_bound: rand(50..99)*100, wage_lower_bound: rand(10..49)*10, is_hidden: "false"])
 end
-puts "10 Public jobs created."
+puts "3 Public jobs created."
+
+create_jos = for i in 1..3 do
+  Job.create!([title: "Job no.#{i} 帮忙遛狗", description: "這是用種子建立的第 #{i} 個Public工作", wage_upper_bound: rand(50..99)*100, wage_lower_bound: rand(10..49)*10, is_hidden: "false"])
+end
+puts "3 Public jobs created."
+
+create_jos = for i in 1..2 do
+  Job.create!([title: "Job no.#{i} 上门煲汤", description: "這是用種子建立的第 #{i} 個Public工作", wage_upper_bound: rand(50..99)*100, wage_lower_bound: rand(10..49)*10, is_hidden: "false"])
+end
+puts "3 Public jobs created."
 
 create_jos = for i in 1..10 do
-  Job.create!([title: "Job no.#{i+10}", description: "這是用種子建立的第 #{i+10} 個Hidden工作", wage_upper_bound: rand(50..99)*100, wage_lower_bound: rand(10..49)*100,is_hidden: "true"])
+  Job.create!([title: "Job no.#{i+10} 面谈", description: "這是用種子建立的第 #{i+10} 個Hidden工作", wage_upper_bound: rand(50..99)*100, wage_lower_bound: rand(10..49)*100,is_hidden: "true"])
 end
 puts "10 Hidden jobs created."
