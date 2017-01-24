d36cccd2aaf3c1acfcf91c6ab6c4369a70138478
elizachen <elizachen34@gmail.com>
2017-01-25 07:05:22 +0800

    modify admin/jobs new, edit

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,12 +1,13 @@
 class Job < ApplicationRecord
   validates :title, presence: true
-  validates :wage_upper_bound, presence: true
-  validates :wage_lower_bound, presence: true
-  validates :wage_lower_bound, numericality: { greater_than: 0}
   validates :city, presence: true
   validates :location, presence: true
   validates :field, presence: true
   validates :require_time, presence: true
+  validates :wage_upper_bound, presence: true
+  validates :wage_lower_bound, presence: true
+  validates :wage_lower_bound, numericality: { greater_than: 0}
+
 
   scope :published, -> { where(is_hidden: false) }
   scope :recent, -> { order('created_at DESC') }
